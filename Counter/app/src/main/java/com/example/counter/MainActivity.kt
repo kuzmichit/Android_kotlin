a574708367f5067bdd8311c3894da0dc891142a7
kuzmichit <kuzmichit@gmail.com>
2025-08-19 19:00:26 +0200

    Aggiunta logica del counter

--- Counter/app/src/main/java/com/example/counter/MainActivity.kt
+++ Counter/app/src/main/java/com/example/counter/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.counter
 
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+
 class MainActivity : AppCompatActivity() {
+
+    var counter = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +22,31 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val plus = findViewById<ImageView>(R.id.plus)
+        val minus = findViewById<ImageView>(R.id.minus)
+
+        plus.setOnClickListener {
+            increaseCounter()
+        }
+        minus.setOnClickListener {
+           decreaseCounter()
+        }
+    }
+
+    private fun decreaseCounter() {
+        counter--
+        if (counter < 0) counter = 0
+        updateCounterText()
+    }
+
+    private fun increaseCounter() {
+        counter++
+        updateCounterText()
+    }
+
+    private fun updateCounterText() {
+        val counterTextView = findViewById<TextView>(R.id.counterText)
+        counterTextView.text = counter.toString()
     }
 }
